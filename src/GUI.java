60f09d9b76616f9ffa9de612447a715289f59c6c
amensiko <anastasija.mensikova@trincoll.edu>
2019-02-05 00:56:53 -0500

    load/store

--- src/GUI.java
+++ src/GUI.java
@@ -49,6 +49,9 @@ public class GUI {
     private JLabel ix_label;
     private JLabel i_label;
 
+    public static int[] memory = new int[2048];
+    public static int[] ix_vals = new int[3];
+
     public GUI() {
         buttonStart.addActionListener(new ActionListener() {
             @Override
@@ -59,13 +62,11 @@ public class GUI {
         r0_textField.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
             }
         });
         r1_textField.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
             }
         });
         clearButton.addActionListener(new ActionListener() {
@@ -74,5 +75,94 @@ public class GUI {
 
             }
         });
+        singleStepButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        haltButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
+
+    public void ipl(String req) {
+        if (req.length() > 16) {
+            resultsTextArea.append("Illegal request, machine fault\n");
+            mfr_textField.setText("1");
+        }
+
+        opcode_textField.setText(req.substring(0, 6));
+        ix_textField.setText(req.substring(8, 10));
+        i_textField.setText("" + req.charAt(10));
+        address_textField.setText(req.substring(11, 16));
+
+        if (Integer.parseInt(req.substring(11, 16), 2) > 32) {
+            mfr_textField.setText("3, illegal address");
+        } else if (Integer.parseInt(req.substring(0, 6), 2) == 0) {
+            resultsTextArea.append("Halting");
+        }
+
+        int opcode = Integer.parseInt(req.substring(0, 6), 2);
+        int gpr = Integer.valueOf(req.substring(6, 8));  //general purpose register: R0, R1, R2, or R3
+        int ix = Integer.valueOf(req.substring(8, 10));
+        int ix_decimal = Integer.parseInt(req.substring(8, 10), 2);
+        int i = Integer.valueOf(req.substring(10, 11));
+        String address_binary = req.substring(11, 16);
+        int address = Integer.valueOf(req.substring(11, 16));
+        int address_decimal = Integer.parseInt(req.substring(11, 16), 2);
+
+        int eff_addr = calculateEA(ix, i, address_decimal);
+
+        if (opcode == 1) {
+            ldrInstr(gpr, eff_addr, i);
+        } else if (opcode == 2) {
+            strInstr(gpr, eff_addr, i);
+        } else if (opcode == 3) {
+            ldaInstr(gpr, eff_addr);
+        } else if (opcode == 41) {
+            ldxInstr(ix, eff_addr, i);
+        } else if(opcode==42) {
+            stxInstr(ix, eff_addr, i);
+        }
+
+
+    }
+
+    public int calculateEA(int ix, int i, int addr) {
+        int eff_addr = 0;
+        if (i != 0) {
+            if (ix == 0) {
+                eff_addr = memory[addr];
+            }
+            if (ix == 1) {
+                eff_addr = memory[ix_vals[0] + addr];
+            }
+            if (ix == 2) {
+                eff_addr = memory[ix_vals[1] + addr];
+            }
+            if (ix == 3) {
+                eff_addr = memory[ix_vals[2] + addr];
+            }
+        } else if (i == 0) {
+            if (ix == 0) {
+                eff_addr = addr;
+            }
+            if (ix == 1) {
+                eff_addr = addr + ix_vals[0];
+            }
+            if (ix == 2) {
+                eff_addr = addr + ix_vals[1];
+            }
+            if (ix == 3) {
+                eff_addr = addr + ix_vals[2];
+            }
+        }
+        return eff_addr;
+    }
+
+
 }
---
+++ src/Simulation.java
@@ -0,0 +1,9 @@
+public class Simulation {
+
+    public static void main(String[] args) {
+        GUI simulation = new GUI();
+        //simulation.setVisible(true);
+
+    }
+
+}
